d7e8e0184429fb4de7aa4bba78d3dbce3ab21bae
Akash Lakkad <akashlakkad5@gmail.com>
2024-12-01 21:39:25 +0530

    Added Routes code

---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const AuthController = require('../controllers/auth');
+const { apiHandler } = require("../utils/payload");
+const { auth } = require("../middleware");
+
+/** Register Route */
+router.post('/register', apiHandler(AuthController.register));
+
+/** login route */
+router.post('/login', apiHandler(AuthController.login));
+
+/** OTP Route */
+router.post('/verifynow', apiHandler(AuthController.verifyNow));
+
+/** Change Password */
+router.put('/changepassword', auth, apiHandler(AuthController.changePassword));
+
+/** Register Company */
+router.post('/create-company', auth, apiHandler(AuthController.createCompany));
+
+/** Get Workspace List */
+router.post('/workspace-list', auth, apiHandler(AuthController.getWorkspaceList));
+
+router.post('/dashboard', auth, apiHandler(AuthController.getDashboard));
+
+module.exports = router;
